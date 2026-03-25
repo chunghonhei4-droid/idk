0262426725634d6b2642708f5d76f52c049deecc
chunghonhei4-droid <chunghonhei4@cloud.qes.edu.hk>
2026-03-25 03:26:51 +0000

    wkiawjdawklhedwa

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
+
+dictionary = {}
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route('/addword', methods=['POST'])
+def handle_json_data():
+    data = request.get_json()
+    dictionary[word] = data
+
+@app.route('/words')
+def getwords():
+    return jsonify(dictionary)
